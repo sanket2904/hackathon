a7a7e052d156414d7cd52a61dd4817227ffef52b
sanket2904 <56267334+sanket2904@users.noreply.github.com>
2022-10-08 13:57:03 -0700

    fifth

--- components/header.js
+++ components/header.js
@@ -10,7 +10,7 @@ export default function Header(props) {
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <a href="https://flowbite.com/" className="flex items-center">
             {/* <Image src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"> */}
-              <span className="self-center text-xl font-semibold whitespace-nowrap ">FIP</span>
+              <span className="self-center text-xl font-semibold whitespace-nowrap ">GENERATION CONNECTION</span>
           </a>
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
--- components/testamonials.jsx
+++ components/testamonials.jsx
@@ -9,7 +9,7 @@ export default function Testamonials(props) {
             <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r ">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 ">
                     <h3 className="text-lg font-semibold text-gray-900 ">Very easy this was to integrate</h3>
-                    <p className="my-4 font-light">If you care for your time, I hands down would go with this."</p>
+                    <p className="my-4 font-light">If you care for your time, I hands down would go with this</p>
                 </blockquote>
                 <figcaption className="flex justify-center items-center space-x-3">
                     <Image height={80} width={80} className="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture" />
@@ -22,7 +22,7 @@ export default function Testamonials(props) {
             <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-tr-lg border-b border-gray-200 ">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 ">
                     <h3 className="text-lg font-semibold text-gray-900 ">Solid foundation for any project</h3>
-                    <p className="my-4 font-light">Designing with Figma components that can be easily translated to the utility classNamees of Tailwind CSS is a huge timesaver!"</p>
+                    <p className="my-4 font-light">Designing with Figma components that can be easily translated to the utility classNamees of Tailwind CSS is a huge timesaver</p>
                 </blockquote>
                 <figcaption className="flex justify-center items-center space-x-3">
                     <Image height={80} width={80} className="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture" />
