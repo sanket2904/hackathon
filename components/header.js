068bd789ef93d105326cd5aa3c0c0aa7ecc6d0b2
sanket2904 <56267334+sanket2904@users.noreply.github.com>
2022-10-08 14:00:05 -0700

    sixth

--- components/header.js
+++ components/header.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import ham from "../public/ham.png";
 import React from "react";
+import { useRouter } from "next/router";
 export default function Header(props) {
   const [show, setShow] = React.useState(false);
+  const router = useRouter();
   return (
     <>
 
@@ -26,10 +28,10 @@ export default function Header(props) {
               </li>
               
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Login</a>
+                <a onClick={() => router.push("/login")} href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Login</a>
               </li>
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Signup</a>
+                <a onClick={() => router.push("/signup")} href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Signup</a>
               </li>
             </ul>
           </div>
