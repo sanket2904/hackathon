cd5d1f3aa33af3a772d5810d8a6846be40236dc9
sanket2904 <56267334+sanket2904@users.noreply.github.com>
2022-10-08 11:59:57 -0700

    second

---
+++ components/hero2.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Hero2(props) {
+    return (
+        <div className="hero2 w-screen h-screen">
+
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 
 import Hero from "../components/hero"
-
+import Hero2 from "../components/hero2"
 export default function Home() { 
   return (
-    <Hero imageLink="https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" headerText="Lorem ipsum dolor sit amet, consectetur adipisicing elit" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio." />
+    <>
+      <Hero imageLink="https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" headerText="Lorem ipsum dolor sit amet, consectetur adipisicing elit" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio." />
+      <Hero2 />
+    </>
   )  
 }
