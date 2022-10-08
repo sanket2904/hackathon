96db5e01b9985f51a13e8a758afefdabc57e58ce
sanket2904 <56267334+sanket2904@users.noreply.github.com>
2022-10-08 13:53:23 -0700

    fifth

--- components/header.js
+++ components/header.js
@@ -1,28 +1,41 @@
 import Image from "next/image";
 import ham from "../public/ham.png";
+import React from "react";
 export default function Header(props) {
+  const [show, setShow] = React.useState(false);
   return (
     <>
-      <div class="header h-20 flex shadow-md">
-        <div class="m-5">
-          <a>
-            <Image src={ham} alt="logo" width={50} height={50} />
-          </a>
-        </div>
 
-        <div class="flex-grow"></div>
-        <div class="flex w-2/4 items-center">
-          <div class="flex-1 text-center text-2xl font-semibold text-indigo-500 cursor-pointer">
-            <a> Home</a>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="https://flowbite.com/" className="flex items-center">
+            {/* <Image src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"> */}
+              <span className="self-center text-xl font-semibold whitespace-nowrap ">FIP</span>
+          </a>
+          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg onClick={() => setShow(!show)} className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          </button>
+          <div className={`${show ? "" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
+            <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
+              <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">About</a>
+              </li>
+              
+              <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Login</a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Signup</a>
+              </li>
+            </ul>
           </div>
-          <div class="flex-1 text-center text-2xl font-semibold text-indigo-500 cursor-pointer">
-            <a> Tracks</a>
-          </div>
-          <div class="m-5 text-center">
-          
         </div>
-        </div>
-      </div>
+      </nav>
+
     </>
   );
 }
--- components/hero2.jsx
+++ components/hero2.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import Card from "./card"
 export default function Hero2(props) {
     return (
-        <div className="hero2 w-screen h-screen flex  items-center md:justify-evenly md:flex-row p-5 flex-wrap ">
+        <div className="hero2 w-screen  flex  items-center md:justify-evenly md:flex-row p-5 flex-wrap ">
             <Card />
             <Card />
             <Card />
