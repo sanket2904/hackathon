12279ce3bcea7a8a8f016068858fd41cc78f3387
sanket2904 <56267334+sanket2904@users.noreply.github.com>
2022-10-08 10:56:42 -0700

    second

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,11 @@
 
 
-export default function Hero() {
+export default function Hero(props) {
     return (
         <>
-            
+            <div className="bg-white h-sceen w-screen text-black">
+                
+            </div>
         </>
     )
 }
