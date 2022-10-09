71a1f4d414099d7cf55985bf39ab4352bc69f1da
sanket2904 <56267334+sanket2904@users.noreply.github.com>
2022-10-08 18:58:07 -0700

    9

--- components/header.js
+++ components/header.js
@@ -21,10 +21,10 @@ export default function Header(props) {
           <div className={`${show ? "" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
             <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
               <li>
-                <a onClick={() => router.push("/")} href="#" className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
+                <a onClick={() => router.push("/")} href="#" className="block py-2 pr-4 pl-3  rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">Home</a>
               </li>
               <li>
-                <a onClick={() => router.push("/about")} href="#" className="block py-2 pr-4 pl-3 text-white  rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">About</a>
+                <a onClick={() => router.push("/about")} href="#" className="block py-2 pr-4 pl-3   rounded md:bg-transparent md:text-blue-700 md:p-0 " aria-current="page">About</a>
               </li>
               <li>
                 <a onClick={() => router.push("/track")} href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Track</a>
